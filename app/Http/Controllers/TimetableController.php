81c8b1dad1f9a8dc60d881751a0a9e9ac0009ed2
raresnita <rares.nita2001@gmail.com>
2026-01-11 15:15:06 +0200

    feat(controller): add functions for returning timetable data for each type (group, professor, classroom)

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -2,20 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classroom;
 use App\Models\Group;
-//use App\Models\Professor;
 use App\Models\Timetable;
 use App\Models\User;
 
 class TimetableController extends Controller
 {
-    //
     public function groupTimetable(Group $group){
-//        dd($group);
         $timetable = Timetable::with(['subject.professor', 'classroom'])
             ->where('group_id', $group->id)
             ->orderBy('day_of_week')
-            ->orderBy('start_time')
+            ->orderBy('start_hour')
             ->get()
             ->groupBy('day_of_week');
 
@@ -24,20 +22,31 @@ class TimetableController extends Controller
 
     public function professorTimetable(User $professor)
     {
-//        dd($professor);
         if($professor->user_role !== 'prof'){
             abort(404);
         }
 
-        $timetable = Timetable::with(['subject.group', 'classroom'])
+        $timetable = Timetable::with(['subject', 'group', 'classroom'])
             ->whereHas('subject', function ($query) use ($professor) {
                 $query->where('professor_id', $professor->id);
             })
             ->orderBy('day_of_week')
-            ->orderBy('start_time')
+            ->orderBy('start_hour')
             ->get()
             ->groupBy('day_of_week');
 
         return view('professors.timetable', compact('professor', 'timetable'));
+    }
+
+    public function classroomTimetable(Classroom $classroom)
+    {
+        $timetable = Timetable::with(['subject.professor', 'group'])
+            ->where('classroom_id', $classroom->id)
+            ->orderBy('day_of_week')
+            ->orderBy('start_hour')
+            ->get()
+            ->groupBy('day_of_week');
+
+        return view('classrooms.timetable', compact('classroom', 'timetable'));
     }
 }
